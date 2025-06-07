571c7e1e30bfd70c65b1f83dfdc749a2657d6de4
Matt Bolster <bolstermatt@gmail.com>
2025-06-07 12:02:53 +0100

    updated useRaceScraper trying to fix build errors

--- frontend/src/hooks/useRaceScraper.jsx
+++ frontend/src/hooks/useRaceScraper.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
-import { getScraperStatus } from './utils/api';
 
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
